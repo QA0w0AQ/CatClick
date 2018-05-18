19b828fbc3283ba63e9659516063a5a7fb4ec5b9
Luke <baslilonfive@gmail.com>
2018-05-18 14:26:37 -0500

    updated html css and jss for 6 cats

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
-const cata = document.querySelector(".cata");
-const catb = document.querySelector(".catb");
-const count1 = document.querySelector(".count1");
-const count2 = document.querySelector(".count2");
-const span1 = document.querySelector(".cat1")
-const span2 = document.querySelector(".cat2")
+const cat1 = document.querySelector('.cat1');
+const cat2 = document.querySelector('.cat2');
+const cat3 = document.querySelector('.cat3');
+const cat4 = document.querySelector('.cat4');
+const cat5 = document.querySelector('.cat5');
+const cat6 = document.querySelector('.cat6');
+const mainCat = document.querySelector('.cat');
+const catName = document.querySelector('h2');
+const number = document.querySelector('.number');
+const totalNumber = document.querySelector('.totalNumber');
+const cats = [cat1, cat2, cat3, cat4, cat5, cat6];
+const catCounts = [0, 0, 0, 0, 0, 0];
+let currentCat = 0;
 
-
-span1.textContent = "small cat";
-span2.textContent = "another one small cat";
-
-cata.addEventListener('click', function () {
-    count1.textContent++;
-    return changeImg();
+cats.forEach(function (cat, index) {
+    cat.addEventListener('click', function (e) {
+        mainCat.src = e.target.src
+        catName.textContent = e.target.alt
+        currentCat = index;
+        number.textContent = catCounts[currentCat];
+    })
 })
 
-function changeImg() {
-    if (count1.textContent == 10) {
-        return cata.src = "./img/625069434_bf6e0d6ea4_o.jpg"
-    }
-}
-
-catb.addEventListener('click', function () {
-    count2.textContent++;
+mainCat.addEventListener('click', function () {
+    catCounts[currentCat]++;
+    number.textContent = catCounts[currentCat];
+    totalNumber.textContent = catCounts.reduce((a, b) => a + b, 0);
 })
